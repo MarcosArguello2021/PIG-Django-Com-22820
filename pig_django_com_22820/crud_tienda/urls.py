7be7ff6184c194bd5c4e80a834dbf57b3a269f59
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-10-17 22:26:58 -0300

    cree las url en urls.py y las vistas en views.py

--- pig_django_com_22820/crud_tienda/urls.py
+++ pig_django_com_22820/crud_tienda/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index ,name="home"),
+    path('vestimenta', views.vestimenta,name='vestimenta'),
+    path('calzado', views.calzado,name='calzado'),
+    path('accesorios', views.accesorios,name='accesorios'),
+    path('suplementos', views.suplementos,name='suplementos'),
     path('contacto/',views.contacto, name="contacto"),
 ]
--- pig_django_com_22820/crud_tienda/views.py
+++ pig_django_com_22820/crud_tienda/views.py
@@ -8,6 +8,23 @@ def index(request):
     fecha=datetime.now().strftime("%D - %H:%M:%S")
     return render(request, "crud_tienda/index.html",{"fecha_now":fecha})
 
+def vestimenta(request):
+    ropa="Ropa.objects.all().order_by('nombre_ropa')"
+    return render(request,"crud_tienda/vestimenta.html",{"ropa":ropa})
+
+def calzado(request):
+    zapas="Calzado.objects.all().order_by('nombre_calzado')"
+    return render(request,"crud_tienda/calzado.html",{"calzado":zapas})
+
+def accesorios(request):
+    acces="Accesorios.objects.all().order_by('nombre_accesorios')"
+    return render(request,"crud_tienda/accesorios.html",{"accesorios":acces})
+
+def suplementos(request):
+    suplem="Suplementos.objects.all().order_by('nombre_suplementos')"
+    return render(request,"crud_tienda/suplementos.html",{"suplementos":suplem})
+
+
 def contacto(request):
-    contacto = FormContacto()
-    return render(request, "crud_tienda/contacto.html",{"contact":contacto})
+    contact = FormContacto()
+    return render(request, "crud_tienda/contacto.html",{"contact":contact})
