f883de4b2910889fdf5a5528cd45359b730c3f38
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-11-04 00:46:30 -0300

    arreglado menu desplegable

--- pig_django_com_22820/crud_tienda/models.py
+++ pig_django_com_22820/crud_tienda/models.py
@@ -4,12 +4,11 @@ from django.db import models
 # Create your models here.
 
 # Choices
-TALLES = (
-    ('XS', 'Extra Small'),
-    ('S', 'Small'),
-    ('M', 'Medium'),
-    ('L', 'Large'),
-    ('XL', 'Extra Large'),
+CATEGORIAS = (
+    ('VM', 'Vestimenta'),
+    ('CA', 'Calzado'),
+    ('AC', 'Accesorios'),
+    ('SU', 'Suplementos'),
 )
 
 SEXO = (
@@ -17,12 +16,36 @@ SEXO = (
     ('H', 'Hombre'),
 )
 
-CATEGORIAS = (
-    ('VM', 'Vestimenta'),
-    ('CA', 'Calzado'),
-    ('AC', 'Accesorios'),
-    ('SU', 'Suplementos'),
+TALLESVES = (
+    ('XS', 'Extra Small'),
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra Large'),
 )
+
+TALLESZAPMU = (
+    ('34.5', '34.5(3.5 uk)'),
+    ('35.5', '35.5(4 uk)'),
+    ('36', '36(4.5 uk)'),
+    ('38', '38(6 uk)'),
+    ('39', '39(7 uk)'),
+    ('40', '40(8 uk)'),
+    ('41', '41(8.5 uk)'),
+)
+
+TALLESZAPHO = (
+    ('38', '38(6 uk)'),
+    ('39', '39(7 uk)'),
+    ('40', '40(8 uk)'),
+    ('41', '41(8.5 uk)'),
+    ('41.5', '41.5(9 uk)'),
+    ('42', '42(9.5 uk)'),
+    ('43', '43(10 uk)'),
+    ('44', '44(11 uk)'),
+    ('45', '45(11.5 uk)'),
+)
+
 
 SUBCATEGORIA = (
     ('RC', 'Remeras & Chombas'),
@@ -37,10 +60,10 @@ class Item(models.Model):
     nombre = models.CharField(max_length=50, verbose_name='Producto')
     precio = models.FloatField()
     foto = models.ImageField(upload_to='productos')
-    talle = models.CharField(max_length=2, choices=TALLES, blank=True, null=True)
+    categoria = models.CharField(max_length=2, choices=CATEGORIAS)
+    talle = models.CharField(max_length=2,choices=TALLESVES) #elif categoria == 'CA': if sexo == 'M': choices == TALLESZAPMU elif sexo == 'H': choices == TALLESZAPHO), blank=True, null=True)
     info = models.CharField(max_length=250)
     stock = models.IntegerField()
-    categoria = models.CharField(max_length=2, choices=CATEGORIAS)
     subcategoria = models.CharField(max_length=2, choices=SUBCATEGORIA, blank=True, null=True)
     sexo = models.CharField(max_length=1, choices=SEXO, blank=True, null=True)
 
