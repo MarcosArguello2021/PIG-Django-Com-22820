d92a42d298d91e99dcc1558b2cba8420bb17fd80
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-11-29 12:42:36 -0300

    arreglo de contactForm de mail

--- pig_django_com_22820/crud_tienda/forms.py
+++ pig_django_com_22820/crud_tienda/forms.py
@@ -3,19 +3,36 @@ from .models import Calzado, Vestimenta, Suplemento, Accesorio, Opciones_calzado
 
 
 class FormContacto(forms.Form):
-    nombre = forms.CharField(required=False)
-    mail = forms.EmailField()
-    mensaje = forms.CharField(widget=forms.Textarea(attrs={'size':'10'}))
+    nombre = forms.CharField(
+        label='Tu nombre',
+        required=False,
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={'class':'form-control white-text'}) #,'placeholder':'Ingresa tu nombre aquí'})
+    )
+    mail = forms.EmailField(
+        label='Tu email',
+        max_length=150,
+        error_messages={'required':'Por favor completa este campo',},
+        widget=forms.TextInput(
+            attrs={'class':'form-control','type':'email'})
+    )
+    mensaje = forms.CharField(
+        label='Tu mensaje',
+        max_length=600,
+        widget=forms.Textarea(
+            attrs={'class':'form-control md-textarea white-text','rows':3})
+    )
 
 
-class FormCalzado(forms.ModelForm):
+class CalzadoForm(forms.ModelForm):
     
     class Meta:
         model = Calzado
         second_model = Opciones_calzado
         fields = ('nombre','precio','foto','info') #,'sexo','talle','stock') #exclude = 'calzado'
 
-class FormVestimenta(forms.ModelForm):
+class VestimentaForm(forms.ModelForm):
 
     class Meta:
         model = Vestimenta
